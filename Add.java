e1ae623ef321b6ce4e7d8979a269c501f9ec6dd6
keerthanavenkataramanan <vkeerthanavenkat22@gmail.com>
2023-01-21 10:40:37 +0530

    adding add to edubridge

---
+++ Add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String[]args)
+{
+int l,m,n,o,p,sum;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the numbers:\n");
+l=sc.nextInt();
+m=sc.nextInt();
+n=sc.nextInt();
+o=sc.nextInt();
+p=sc.nextInt();
+sum=l+m+n+o+p;
+System.out.println("sum is\t"+sum);
+}
+}
